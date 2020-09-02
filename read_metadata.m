17d0abe437dd42bff3bfc312c6a422ae09c5d27e
David Pedrosa <david.pedrosa@staff.uni-marburg.de>
2020-09-02 11:21:18 +0200

    extension of the control script which selects the subjects to analyse and the adaptation (inclusion of GUI, helper functions to select DD)

---
+++ read_metadata.m
@@ -0,0 +1,30 @@
+function dat = read_metadata(varargin)
+
+%   Function which reads metadata from xls-file; returns a table
+
+%   Copyright (C) September 2020
+%   D. Pedrosa, Emil Pruchnewski, Alexander Sperlich, Josefine Walthaler
+%   University Hospital of Gießen and Marburg
+%
+%   This software may be used, copied, or redistributed as long as it is
+%   not sold and this copyright notice is reproduced on each copy made.
+%   This routine is provided as is without any express or implied
+%   warranties whatsoever.
+
+if nargin == 0
+    if strcmp(getenv('username'), 'dpedrosa')
+        ROOTDIR = 'D:\DBSnetworks';
+        filename_metadata = fullfile(ROOTDIR, 'metadata_DBSnetworks.xlsx');
+    else
+        error('ROOTDIR unknown, please define directory to work at')
+    end
+    
+end
+
+debug = 0;
+if debug == 1
+    detectImportOptions(filename_metadata);
+    keyboard;
+end
+
+dat = readtable(filename_metadata);
