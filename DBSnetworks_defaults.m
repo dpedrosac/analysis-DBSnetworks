6fbfc0fb564d537420e133e9db16c1e1ee2a13a7
charlottestu <76004165+charlottestu@users.noreply.github.com>
2021-06-02 15:02:08 +0200

    Add files via upload

--- DBSnetworks_defaults.m
+++ DBSnetworks_defaults.m
@@ -37,8 +37,8 @@ elseif ispc
         ROOTDIR = 'F:\Alexander\Doktorarbeit\analysis-DBSnetworks-master';
         addpath('C:\Users\User\Documents\Fieldtrip\fieldtrip-20200919'); ft_defaults;
     elseif strcmp(getenv('username'), 'charl')
-        wdir = 'G:\Doktorarbeit\EEG DBSnetworks';
-        ROOTDIR = ['C:\Users\charl\Documents\Studium\Doktorarbeit\analysis-DBSnetworks-master'];
+        wdir = 'G:\Doktorarbeit\analysis-DBSnetworks-master\data';
+        ROOTDIR = 'G:\Doktorarbeit\analysis-DBSnetworks-master';
         addpath('C:\Users\charl\Documents\Studium\Doktorarbeit\fieldtrip-lite-20201205\fieldtrip-20201205'); ft_defaults;
     else
         warning("Please specific folders to 'DBSnetworks_default.m' fitting to your settings")
